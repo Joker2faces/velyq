db462aef01c1645ba9ec7806e645f3bf1deb0208
Joker2face <joker2face1990@gmail.com>
2026-09-04 11:11:06 +0300

    feat: persist quality assessments as append-only history

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -4,4 +4,5 @@ export * from "./repositories/jobs.js";
 export * from "./repositories/observations.js";
 export * from "./repositories/provider-runs.js";
 export * from "./repositories/permissions.js";
+export * from "./repositories/quality.js";
 export * from "./schema/index.js";
